535e8d1ec8fa48932c8019d6b5ceb7b30527c3c5
Alexandr Rodionov <rodionov.alexandr-sergeevich@yandex.ru>
2024-08-23 12:51:26 +0800

    Update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,15 +4,19 @@ import { useState } from "react";
 
 const Form = () => {
   // Массив заголовков табов формы.
-  const headers = [];
+  const headers = [
+    'Заполните анкету чтобы мы могли подобрать вам психолога из сообщества "Хранители"',
+  ];
   return (
     <>
       {/* Не задаём ограничений  т.к ширину будет ограничивать контейнер в Tilda */}
       <div className="bg-white h-full rounded-lg flex flex-col relative w-full">
         <div
           data-name="header-block"
-          className="p-10 bg-black sticky top-0 rounded-t-lg z-20"
-        ></div>
+          className="p-5 bg-black sticky top-0 rounded-t-lg z-20"
+        >
+          <h2 className="text-white font-medium text-base">{headers[0]}</h2>
+        </div>
 
         <div className="relative overflow-y-scroll">
           <Slots></Slots>
@@ -20,7 +24,7 @@ const Form = () => {
 
         <div
           data-name="header-block"
-          className="p-10 rounded-b-lg bg-black z-20"
+          className="p-5 rounded-b-lg bg-black z-20"
         ></div>
       </div>
     </>
