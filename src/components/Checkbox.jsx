b342d53b152692f1ba9cb3ba7f8eb2fe5a4d60f6
Alexandr Rodionov <rodionov.alexandr-sergeevich@yandex.ru>
2024-08-24 20:16:37 +0800

    update checkbox

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Checkbox = ({ children }) => {
+const Checkbox = ({ children, classLabel, ...props }) => {
+  function uuidv4() {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) =>
+      (
+        +c ^
+        (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))
+      ).toString(16)
+    );
+  }
+
+  let checkbox_id = uuidv4();
   return (
     <>
       <input
         type="checkbox"
-        name="happy"
+        id={checkbox_id}
         className="custom-checkbox"
-        value="yes"
+        {...props}
       />
-
-      <label>{children}</label>
+      <label for={checkbox_id} className={classLabel}>
+        {children}
+      </label>
     </>
   );
 };
--- src/survey/WelcomePage.jsx
+++ src/survey/WelcomePage.jsx
@@ -1,9 +1,40 @@
 import React from "react";
 import Checkbox from "../components/Checkbox";
+import { useState } from "react";
+
 const WelcomePage = () => {
+  const [checkedList, setCheckedList] = useState([]);
+
+  function toogleList(elem, list) {
+    let list_copy = list;
+    if (list.includes(elem)) {
+      list.splice(1, list.indexOf(elem));
+    } else {
+      list.push(elem);
+    }
+
+    return list_copy;
+  }
+
+  const anxietyList = [
+    "Низкая самооценка",
+    "Тревога, страхи, панические атаки",
+    "Отверженность, непонимание себя, своих чувств и желаний, своего места в жизни",
+    "Сложности с построением близких и любовных отношений",
+    "Проблемы с построением гармоничных отношений в семье, в коллективе",
+    "Сложности в общении в рабочем коллективе",
+    "Травматическое событие, с которым сложно справиться самому",
+    "Сложности в общении и воспитании детей",
+    "Зависимости: химические, психологические и тд",
+    "Выход из абьюзивных отношений",
+    "Эмоциональные всплески и эмоциональная пустота, агрессия и гнев",
+    "Выгорание: эмоциональное и профессиональное",
+    "Детские травмы, которые сильно сказываются на взрослой жизни",
+  ];
+
   return (
     <div className="px-5 py-12 flex flex-col gap-8">
-      <div data-name="question-block flex flex-col gap-4">
+      <div data-name="question-block flex flex-col">
         <div className="flex flex-col gap-1">
           <h3 className="font-medium text-lg text-dark-green">
             Что вас беспокоит?
@@ -13,10 +44,12 @@ const WelcomePage = () => {
           </p>
         </div>
       </div>
-      <ul data-name="question-inputs flex flex-col">
-        <li>
-          <Checkbox>Низкая самооценка</Checkbox>
-        </li>
+      <ul data-name="question-inputs">
+        {anxietyList.map((anxiety) => (
+          <li className="mt-2">
+            <Checkbox>{anxiety}</Checkbox>
+          </li>
+        ))}
       </ul>
     </div>
   );
