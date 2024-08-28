7b9d5929da4c1e4ab100672f8c49ba1272dc2ee1
Alexandr Rodionov <rodionov.alexandr-sergeevich@yandex.ru>
2024-08-28 18:49:54 +0800

    Update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -136,59 +136,58 @@ const Form = ({ maxTabsCount }) => {
           {activeTabIndex == 5 && <Slots></Slots>}
           {activeTabIndex == 6 && <AskContacts></AskContacts>}
           {activeTabIndex == 7 && <Name></Name>}
-        </div>
+          {/* Control buttons  */}
+          <div
+            data-name="control-block"
+            className="p-5 flex items-center sticky bottom-0 flex-wrap-reverse gap-4 bg-[#2c3531] w-full z-30"
+          >
+            {activeTabIndex != 0 ? (
+              <Button
+                size="small"
+                intent="cream-transparent"
+                hower="primary"
+                className="sm:max-w-64 max-sm:min-w-40 mr-auto"
+                onClick={() => {
+                  setActiveTabIndex(activeTabIndex - 1);
+                }}
+              >
+                Назад
+              </Button>
+            ) : (
+              ""
+            )}
+            {activeTabIndex != maxTabsCount - 1 ? (
+              <Button
+                size="small"
+                intent="cream"
+                hower="primary"
+                className="sm:max-w-64 max-sm:min-w-40 ml-auto"
+                onClick={() => {
+                  showNextTab(activeTabIndex);
+                }}
+              >
+                Вперёд
+              </Button>
+            ) : (
+              ""
+            )}
 
-        {/* Control buttons  */}
-        <div
-          data-name="control-block"
-          className="p-5 flex items-center sticky bottom-0 flex-wrap-reverse gap-4 bg-[#2c3531] w-full z-30"
-        >
-          {activeTabIndex != 0 ? (
-            <Button
-              size="small"
-              intent="cream-transparent"
-              hower="primary"
-              className="sm:max-w-64 max-sm:min-w-40 mr-auto"
-              onClick={() => {
-                setActiveTabIndex(activeTabIndex - 1);
-              }}
-            >
-              Назад
-            </Button>
-          ) : (
-            ""
-          )}
-          {activeTabIndex != maxTabsCount - 1 ? (
-            <Button
-              size="small"
-              intent="cream"
-              hower="primary"
-              className="sm:max-w-64 max-sm:min-w-40 ml-auto"
-              onClick={() => {
-                showNextTab(activeTabIndex);
-              }}
-            >
-              Вперёд
-            </Button>
-          ) : (
-            ""
-          )}
-
-          {activeTabIndex == maxTabsCount - 1 ? (
-            <Button
-              size="small"
-              intent="cream"
-              hower="primary"
-              className="sm:max-w-64 max-sm:min-w-40 ml-auto"
-              onClick={() => {
-                sendData();
-              }}
-            >
-              Отправить форму
-            </Button>
-          ) : (
-            ""
-          )}
+            {activeTabIndex == maxTabsCount - 1 ? (
+              <Button
+                size="small"
+                intent="cream"
+                hower="primary"
+                className="sm:max-w-64 max-sm:min-w-40 ml-auto"
+                onClick={() => {
+                  sendData();
+                }}
+              >
+                Отправить форму
+              </Button>
+            ) : (
+              ""
+            )}
+          </div>
         </div>
       </div>
     </>
