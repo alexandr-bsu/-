d42ae78af2f54da83ba0560b79eb6e1a301e6703
Alexandr Rodionov <rodionov.alexandr-sergeevich@yandex.ru>
2025-03-07 15:32:04 +0300

    Update FormSlotsReassigment.jsx

--- src/components/FormSlotsReassigment.jsx
+++ src/components/FormSlotsReassigment.jsx
@@ -41,7 +41,7 @@ const FormSlotsReassigment = ({ maxTabsCount }) => {
   const [showError, setShowError] = useState(false);
 
   // Массив заголовков табов формы.
-  const headers = ["Заявка на подбор психолога из сообщества Хранители"];
+  const headers = ["Перезапись к психологу из сообщества Хранители"];
   const [activeTabIndex, setActiveTabIndex] = useState(0);
   const areSlotsEmpty = useSelector((state) => state.form.emptySlots);
 
